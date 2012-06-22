e752c655e3eea58657fa9e51413f3310b908848b
Neil Roberts <neil@linux.intel.com>
2012-06-22 12:04:02 +0100

    Add a module for cogl-pango

--- cogl-Android.mk
+++ cogl-Android.mk
@@ -12,12 +12,14 @@ LOCAL_STATIC_LIBRARIES := glib-android gmodule gobject gthread
 include $(PREBUILT_STATIC_LIBRARY)
 
 # cogl-pango
-#include $(CLEAR_VARS)
-#
-#LOCAL_MODULE        := cogl-pango
-#LOCAL_SRC_FILES     := lib/libcogl-pango.a
-#LOCAL_EXPORT_CFLAGS := -I$(LOCAL_PATH)/include/cogl
-#
-#include $(PREBUILT_STATIC_LIBRARY)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE        := cogl-pango
+LOCAL_SRC_FILES     := lib/libcogl-pango2.a
+LOCAL_EXPORT_CFLAGS := -I$(LOCAL_PATH)/include/cogl2
+LOCAL_STATIC_LIBRARIES := pangocairo cogl
+
+include $(PREBUILT_STATIC_LIBRARY)
 
 $(call import-module,glib-android)
+$(call import-module,pango)
